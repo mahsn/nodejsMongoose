2278f509e5e9ee786fa476af9b8e73cd1618b431
Mac <mac@MacBook-Pro-van-Mac.local>
2021-08-20 21:33:58 +0200

    change moongodb url

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://admin:admin@cluster0.aejb5.mongodb.net/mongoose?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
